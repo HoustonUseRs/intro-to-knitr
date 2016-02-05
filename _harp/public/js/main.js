db5e80619a4f1050b3483a0b8f8258d19dbedb6b
pandafulmanda <pandafulmanda@gmail.com>
2016-02-05 00:43:22 -0600

    quick toc on side

--- _harp/public/js/main.js
+++ _harp/public/js/main.js
@@ -8,30 +8,79 @@
 
   function initialize(){
     setUpExternalLinks();
+    setUpToc('.article-docs', '#toc-holder');
   }
 
+  function setUpToc(articleSelector, tocSelector){
+    var tocLinkTemplate = (function(){
+      var listItem = document.createElement('li');
+      var link = document.createElement('a');
+      listItem.appendChild(link);
+      return listItem;
+    }());
+
+    var headings = document.querySelectorAll(articleSelector + ' h1');
+    var tocHolder = document.querySelector(tocSelector);
+
+    var toc = _.map(headings, function(heading){
+      var linkInfo = makeHeadingNavigable(heading);
+      var listItem = makeTocLink(linkInfo);
+      tocHolder.appendChild(listItem);
+    });
+
+    function makeHeadingNavigable(heading){
+      var aName = _.kebabCase(heading.innerText);
+      heading.id = aName;
+
+      return {name: aName, label: heading.innerText};
+    }
+
+    function makeTocLink(linkInfo){
+      var base = tocLinkTemplate.cloneNode(true);
+
+      base.childNodes[0].innerText = linkInfo.label;
+      base.childNodes[0].href = '#' + linkInfo.name;
+      base.addEventListener('click', setActiveItem);
+
+      return base;
+    }
+
+    function setActiveItem(clickEvent){
+      var currentActive = document.querySelector(tocSelector + ' li.active');
+      if(currentActive){
+        currentActive.classList.remove('active');        
+      }
+      this.classList.add('active');
+    }
+
+  }
 
   function setUpExternalLinks(){
     var links = document.querySelectorAll('a[href^="http"]');
 
-    Array.prototype.forEach.call(links, function(link){
+    _.each(links, function(link){
       if(isLinkExternal(link.href)){
-        link.target = '_blank';
+        openLinkInNewTab(link);
       }
     });
-  }
 
-  function isLinkExternal(url){
-    var domainRe = /https?:\/\/((?:[\w\d\-]+\.)+[\w\d\-]{2,})/i;
-
-    function domain(url) {
-      var domainSplit = domainRe.exec(url);
-      if(domainSplit){
-        domainSplit = domainSplit[1];
-      }
-      return domainSplit;
+    function openLinkInNewTab(link){
+      link.target = '_blank';
     }
-    return domain(location.href) !== domain(url);
+
+    function isLinkExternal(url){
+      var domainRe = /https?:\/\/((?:[\w\d\-]+\.)+[\w\d\-]{2,})/i;
+
+      function domain(url) {
+        var domainSplit = domainRe.exec(url);
+        if(domainSplit){
+          domainSplit = domainSplit[1];
+        }
+        return domainSplit;
+      }
+      return domain(location.href) !== domain(url);
+    }
   }
+
 
 }(window))
